0c267a331e024b7338d72812f1a825a2390f1f1f
Dario Dabovic <dabovic.d02@htlwienwest.at>
2021-01-12 19:07:18 +0100

    added Constructor class Account

--- Git_Basics/Account.cs
+++ Git_Basics/Account.cs
@@ -7,7 +7,12 @@ namespace Git_Basics
     class Account
     {
         public string Email { get; set; }
-
         public string Passwort { get; set; }
+
+        public Account(string email, string passwort)
+        {
+            Email = email;
+            Passwort = passwort;
+        }
     }
 }
